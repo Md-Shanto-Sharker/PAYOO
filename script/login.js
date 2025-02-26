8705eaf6c0bb6b85511015ec30f4729c232a0a99
SHANTO <rabbymd55555@gmail.com>
2025-02-26 23:09:23 +0600

    added html and css styles

---
+++ script/login.js
@@ -0,0 +1,21 @@
+document.getElementById('login-btn').addEventListener('click',function(event){
+    event.preventDefault();
+    const accountNumber = document.getElementById('account-number').value;
+   
+    const Pin = document.getElementById('pin').value;
+    const convertedPin = parseInt(Pin)
+    if(accountNumber.length ===11){
+        
+        if(convertedPin===1234){
+            window.location.href="./main.html"
+        }else{
+            alert('incorrect pin');
+        }
+    }else{
+        alert('incorrect number');
+    }
+})
+
+
+
+
