5ec197726086fe153d4e643bd4c1818cb42186d5
SHANTO <rabbymd55555@gmail.com>
2025-03-03 09:10:44 +0600

    added addmoney.js file

--- script/login.js
+++ script/login.js
@@ -1,21 +1,18 @@
-document.getElementById('login-btn').addEventListener('click',function(event){
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
     event.preventDefault();
-    const accountNumber = document.getElementById('account-number').value;
-   
-    const Pin = document.getElementById('pin').value;
-    const convertedPin = parseInt(Pin)
-    if(accountNumber.length ===11){
-        
-        if(convertedPin===1234){
-            window.location.href="./main.html"
-        }else{
-            alert('incorrect pin');
-        }
-    }else{
-        alert('incorrect number');
+    const accountNumber = document.getElementById("account-number").value;
+
+    const Pin = document.getElementById("pin").value;
+    const convertedPin = parseInt(Pin);
+    if (accountNumber.length === 11) {
+      if (convertedPin === 1234) {
+        window.location.href = "./main.html";
+      } else {
+        alert("incorrect pin");
+      }
+    } else {
+      alert("incorrect number");
     }
-})
-
-
-
-
+  });
