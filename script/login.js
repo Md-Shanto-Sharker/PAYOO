2f1d6a7bbbb43731ed165989d5cb9799d8e872c3
SHANTO <rabbymd55555@gmail.com>
2025-03-21 11:22:26 +0600

    added addmoney fetures

--- script/login.js
+++ script/login.js
@@ -1,16 +1,17 @@
-document.getElementById('login-btn').addEventListener('click',function(event){
-    event.preventDefault()
-    const accountNumber = document.getElementById('Account-number').value;
-    const pinNumber = document.getElementById('pin').value;
-    const convertedPin = parseInt(pinNumber)
-    if(accountNumber.length===11){
-        if(convertedPin===1234){
-            window.location.href="./main.html"
-            console.log('rong pin');
-        }else{
-            alert('wrong pin')
-        }
-    }else{
-        alert('Invalid number')
+document
+  .getElementById("login-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const acountNunber = document.getElementById("Acount-nunber").value;
+    const pin = document.getElementById("pin").value;
+    const convertedPin = parseInt(pin);
+    if (acountNunber.length === 11) {
+      if (convertedPin === 1234) {
+        window.location.href = "./main.html";
+      } else {
+        alert("pin tikh nai ");
+      }
+    } else {
+      alert("need valid acount number");
     }
-})
+  });
